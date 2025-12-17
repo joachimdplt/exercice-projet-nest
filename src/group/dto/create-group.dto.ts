1182f507ebcc0f7253782ad29a8f98c29a0edd18
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-12-17 20:06:04 +0100

    add rules to group.dto

--- src/group/dto/create-group.dto.ts
+++ src/group/dto/create-group.dto.ts
@@ -1,1 +1,15 @@
-export class CreateGroupDto {}
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+/// Le DTO ne founir que ce que l'utilisateur doit fournir
+
+export class CreateGroupDto {
+  @IsNotEmpty()
+  @IsString()
+  name!: string;
+
+  @IsInt()
+  formationId!: number;
+
+  @IsInt()
+  leadId!: number; // TeamMemberId
+}
