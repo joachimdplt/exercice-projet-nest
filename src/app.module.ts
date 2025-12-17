aea72da6f7d56175befd7f938c2a770d0fd2ebb7
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-12-17 17:32:00 +0100

    generetae ressource post et ajout des imorts auto dans amm°modeul

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FormationModule } from './formation/formation.module';
 import { PrismaModule } from 'prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [FormationModule, PrismaModule],
+  imports: [FormationModule, PrismaModule, UserModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
