5482403e7640a1045a3a3469914a7688098b4bda
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-12-18 17:57:44 +0100

    add ConfigModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { PrismaModule } from 'prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { GroupModule } from './group/group.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [FormationModule, PrismaModule, UserModule, PostModule, GroupModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), FormationModule, PrismaModule, UserModule, PostModule, GroupModule],
   controllers: [AppController],
   providers: [AppService],
 })
